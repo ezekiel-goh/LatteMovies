f8b4f5bb85a94053add792436d8c8775401b5c83
bueblerries <ezekielgoh05.22@ichat.sp.edu.sg>
2023-05-16 14:45:53 +0800

    Project LatteMovies Initalisa tion
    clean slate

---
+++ models/modules copy.js
@@ -0,0 +1,11 @@
+//-- Edit this code Accordingly, can refer to modulesOld.js
+
+const { query } = require('../database');
+const {
+    DUPLICATE_ENTRY_ERROR,
+    EMPTY_RESULT_ERROR,
+    MYSQL_ERROR_CODE,
+    TABLE_ALREADY_EXISTS_ERROR,
+} = require('../errors');
+
+
