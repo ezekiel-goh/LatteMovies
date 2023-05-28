4e1763414795fb5b62bc8d10100a9f3827e1ed5a
heidelborja <heidelbergborja.22@ichat.sp.edu.sg>
2023-05-28 21:38:31 +0800

    Added CRUD for user.js (not working yet)

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const { getConnection } = require('./databaseConnection');
+const { SqlLiteErrorCodes, DuplicateEntryError, NotFoundError } = require('./errors.js');
+
+module.exports.getUser = function getUser(UserID) {
+    const connection = getConnection();
+    return connection.get('SELECT * FROM User WHERE UserID = ?', [UserID]).then((User) => {
+        if (!User) throw new NotFoundError(`User ${UserID}`);
+        return User;
+    });
+};
+
+module.exports.addUser = function addUser(UserID, name) {
+    const connection = getConnection();
+    return connection
+        .run('INSERT INTO User (UserID, name) VALUES (?, ?)', [UserID, name])
+        .then(() => {
+            return;
+        })
+        .catch((error) => {
+            if (error.errno === SqlLiteErrorCodes.SQLITE_CONSTRAINT) throw new DuplicateEntryError(`User ${UserID}`);
+        });
+};
+
+module.exports.updateUser = function updateUser(UserID, name) {
+    const connection = getConnection();
+    return connection.run('UPDATE User SET name = ? WHERE UserID = ?', [name, UserID]).then((response) => {
+        if (!response.changes) throw new NotFoundError(`User ${UserID}`);
+        return;
+    });
+};
+
+module.exports.deleteUser = function deleteUser(UserID) {
+    const connection = getConnection();
+    return connection.run('DELETE FROM User WHERE UserID = ?', [UserID]).then((response) => {
+        if (!response.changes) throw new NotFoundError(`User ${UserID}`);
+        return;
+    });
+};
